de92a8fedeabb5fcbfae5861cab657f87b26d141
mu5tafacool <mustafakulf1@gmail.com>
2022-07-30 23:38:40 +0300

    30 07 22 23:55

---
+++ src/test/java/get_requests/Get05.java
@@ -0,0 +1,51 @@
+package get_requests;
+
+import base_urls.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Get05 extends HerOkuAppBaseUrl {
+
+     /*
+        Given
+            https://restful-booker.herokuapp.com/booking
+        When
+            User send a request to the URL
+        Then
+            Status code is 200
+	  	And
+	  		Among the data there should be someone whose firstname is "Aaron" and last name is "Chen"
+     */
+
+    @Test
+    public void get01() {
+
+        //1. Step: Set the Url
+
+        //https://restful-booker.herokuapp.com/booking?firstname=GGS&lastname=FINCH
+        //https://restful-booker.herokuapp.com/booking/1594?firstname=Sally&lastname=Brown
+        spec.pathParam("first", "booking").
+                queryParams("firstname", "Sally",
+                        "lastname", "Brown");
+
+        //2. Step: Set the expected data
+
+        //3. Step: Send the request and get the response
+
+        Response response = given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
+
+        //4. Step: Do Assertion
+        response.then().assertThat().statusCode(200);
+        assertTrue(response.asString().contains("bookingid"));
+
+
+    }
+
+}
